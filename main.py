615f29b1a8c5995cca1eca4098b9d422c65ee139
EugeneChurylov <e.churylov@gmail.com>
2024-03-23 22:22:58 +0100

    not needed frameworks removed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import mimetypes
-import json
 import socket
 import logging
 from urllib.parse import urlparse, unquote_plus
@@ -7,7 +6,6 @@ from pathlib import Path
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from multiprocessing import Process
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from datetime import datetime
 
 URI = "mongodb://127.0.0.1:27017"
